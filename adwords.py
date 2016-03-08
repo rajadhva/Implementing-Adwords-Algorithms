de1d6eb3411d0e23daea6d70a051385b194c37e1
rajadhva <vaibhav.rajadhyaksha@gmail.com>
2016-03-08 01:13:19 -0500

    imlemented the partial logic for greedy algorithm

--- adwords.py
+++ adwords.py
@@ -4,10 +4,11 @@ import xlrd
 import csv
 import random
 import sys
+
 f = open('queries.txt', 'r+')
 queries=[]
 for line in f:
-    queries.append(line)
+    queries.append(line.rstrip())
     print line
 
 #c = csv.writer(open("bidder_dataset.csv", "wb"))
@@ -22,13 +23,13 @@ advertiser_budget={}
 
 for row in cr:
     if(query_advertiser.has_key(row[1])):
-       query_advertiser[row[1]][row[0]]=row[2]
+       query_advertiser[row[1]][row[0]]=float(row[2])
     else:
        query_advertiser[row[1]]={}
-       query_advertiser[row[1]][row[0]]=row[2]
+       query_advertiser[row[1]][row[0]]=float(row[2])
 
     if(row[3] not in (None, "")):
-        advertiser_budget[row[0]]=row[3]
+        advertiser_budget[row[0]]=float(row[3])
 
 
 #for key in query_advertiser.keys():
@@ -38,27 +39,38 @@ for row in cr:
     #print key,advertiser_budget[key]
 
 
-greedy_array=greedy(query_advertiser,advertiser_budget,queries)
+
 
 def greedy(query_advertiser,advertiser_budget,queries):
-    for i in range(0,100):
-        random.seed(0)
-        queries=random.shuffle(queries)
-        current_budget=advertiser_budget
-        for query in queries:
-            advertisers=query_advertiser[query].keys()
-            advertisers=filter(advertisers,current_budget)
-            if not advertisers:
-                continue
-            max=-sys.maxint-1
-            for
+    revenue=0
+    #random.seed(0)
+    #random.shuffle(queries)
+    current_budget=advertiser_budget
 
+    for query in queries:
+        print query
+        advertisers=query_advertiser[query].keys()
+        advertisers=filter(advertisers,current_budget,query_advertiser,query)
+        if not advertisers:
+            continue
+        max=-sys.maxint-1
+        for advertiser in advertisers:
+            if(query_advertiser[query][advertiser] > max):
+                max=query_advertiser[query][advertiser]
+                temp_advertiser=advertiser
+            if(query_advertiser[query][advertiser]>max):
+                if(advertiser<temp_advertiser):
+                    max=query_advertiser[query][advertiser]
+                    temp_advertiser=advertiser
+        current_budget[temp_advertiser]= current_budget[temp_advertiser]-max
+        revenue=revenue+max
+    return revenue
 
-def filter(advertisers,current_budget):
+def filter(advertisers,current_budget,query_advertiser,query):
     for name in advertisers:
-        if(current_budget[advertisers]==0):
+        if(current_budget[name]==0 or current_budget[name]-query_advertiser[query][name]<0):
             advertisers.remove(name)
 
     return advertisers
 
-def shuffle(temp3):
+print greedy(query_advertiser,advertiser_budget,queries)
