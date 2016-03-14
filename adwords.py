0e128a8d24bbd9a786ff9a3269c3b3c3677e412a
rajadhva <vaibhav.rajadhyaksha@gmail.com>
2016-03-13 21:02:52 -0400

    added the mssv algorithms

--- adwords.py
+++ adwords.py
@@ -4,6 +4,7 @@ __author__ = 'Vaibhav'
 import csv
 import random
 import sys
+import math
 import operator
 f = open('queries.txt', 'r+')
 optimal_revenue=0.0
@@ -31,18 +32,6 @@ for row in cr:
     if(row[3] not in (None, "")):
         advertiser_budget[row[0]]=float(row[3])
 
-    #print advertiser_budget
-#for key1 in query_advertiser.keys():
-            #print query_advertiser[key1]
-
-#for i in advertiser_budget.keys():
-    #print str(i) + " : " + str(advertiser_budget[i])
-
-#for key in query_advertiser.keys():
-    #print key,query_advertiser[key]
-
-#for key in advertiser_budget.keys():
-    #print key,advertiser_budget[key]
 
 for i in advertiser_budget.values():
     optimal_revenue= optimal_revenue + i
@@ -52,8 +41,6 @@ def greedy(query_advertiser,advertiser_budget,queries):
 
     revenue=[]
     main_revenue=0
-    flag=False
-
     for i in range(100):
         if(i!=0):
             random.seed(0)
@@ -135,6 +122,48 @@ def balance(query_advertiser,advertiser_budget,queries):
 
     return main_revenue,average_revenue/100
 
+def mssv(query_advertiser,advertiser_budget,queries):
+    revenue=[]
+    main_revenue=0
+    total_budget=advertiser_budget.copy()
+    for i in range(100):
+        print i
+        if(i!=0):
+            random.seed(0)
+            random.shuffle(queries)
+        #print advertiser_budget
+        current_budget=advertiser_budget.copy()
+        temp_revenue=0
+        for query in queries:
+            advertisers=query_advertiser[query].keys()
+            for advertiser in advertisers:
+                if(current_budget[advertiser]==0):
+                    print "Came Here"
+                    advertiser.remove(advertiser)
+            if not advertisers:
+                continue
+            max=0
+            temp_advertiser = -1
+            for advertiser in advertisers:
+                if current_budget[advertiser] - query_advertiser[query][advertiser] > 0:
+                    #print total_budget[advertiser]-current_budget[advertiser]
+                    ratio=(query_advertiser[query][advertiser]*(1-(math.exp((total_budget[advertiser]-current_budget[advertiser])/total_budget[advertiser]-1))))
+                    #print ratio
+                    if(ratio>max):
+                        max=ratio
+                        temp_advertiser=advertiser
+
+            if temp_advertiser != -1:
+                current_budget[temp_advertiser] = current_budget[temp_advertiser] - query_advertiser[query][temp_advertiser]
+                temp_revenue= temp_revenue + query_advertiser[query][temp_advertiser]
+
+        revenue.append(temp_revenue)
+    average_revenue=0
+    for i in revenue:
+        average_revenue= average_revenue+i
+
+    return main_revenue,average_revenue/100
+
 
 #driver functions to fetch output and print them:
 def greedyOutput():
@@ -143,6 +172,13 @@ def greedyOutput():
    print "The revenue in greedy is" + " : " + str(average_revenue)
    print "The competitive ratio in greedy is" + " : " + str(average_revenue/optimal_revenue)
 
+def mssvoutput():
+   main_revenue,average_revenue=mssv(query_advertiser,advertiser_budget,queries)
+   #print optimal_revenue
+   print "The revenue in mssv is" + " : " + str(average_revenue)
+   print "The competitive ratio in mssv is" + " : " + str(average_revenue/optimal_revenue)
+
+
 def balanceoutput():
    main_revenue,average_revenue=balance(query_advertiser,advertiser_budget,queries)
    #print optimal_revenue
@@ -150,7 +186,11 @@ def balanceoutput():
    print "The competitive ratio in balance is" + " : " + str(average_revenue/optimal_revenue)
 
 
+
+#calling  the functons to print the output
+
 greedyOutput()
+mssvoutput()
 balanceoutput()
 
 
